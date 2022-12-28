e5f0cc08b129ad2e1644333c4a135fffdfe09145
BadJui <105153424+BadJui@users.noreply.github.com>
2022-12-28 17:24:15 +0800

    Add files via upload

---
+++ static/js/bd.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fb7e760e987871d56396999d288238a4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ static/js/hm.js
@@ -0,0 +1,6 @@
+ (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.woozooo.com/hm.js?v=1.2";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  	})();
