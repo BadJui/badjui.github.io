666e68f071dbbf5a309203760f7153b8d164ff9e
BadJui <105153424+BadJui@users.noreply.github.com>
2023-04-07 19:50:47 +0800

    Delete static/js directory

--- static/js/hm.js
+++
@@ -1,6 +0,0 @@
- (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.woozooo.com/hm.js?v=1.2";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-  	})();
